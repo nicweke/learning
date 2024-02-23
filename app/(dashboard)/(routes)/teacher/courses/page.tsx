858413b06433ba584df2a8cb6872373c78df52d7
nicweke <nicweke@gmail.com>
2024-02-23 10:02:16 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,29 @@
+import { auth } from "@clerk/nextjs";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+const Courses = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <DataTable columns={columns} data={courses} />
+    </div>
+  );
+};
+
+export default Courses;
